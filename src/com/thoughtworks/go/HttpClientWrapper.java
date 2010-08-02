c26e96acd812417d645e5244864894375b8da990
Pavan <itspanzi@gmail.com>
2010-08-02 17:42:23 +0530

    Implemented the HttpClientWrapper. Also added a simple spike class which demostrates how this works.

--- src/com/thoughtworks/go/HttpClientWrapper.java
+++ src/com/thoughtworks/go/HttpClientWrapper.java
@@ -1,18 +1,59 @@
 package com.thoughtworks.go;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.io.IOException;
+
 /**
- * ADD_UNDERSTANDS_BLOCK
+ * @understands communicating with the Go server using http
  */
 public class HttpClientWrapper {
+    private final String hostname;
+    private final int port;
+    private final String username;
+    private final String password;
+
+    public HttpClientWrapper(String hostname, int port, String username, String password) {
+        this.hostname = hostname;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+    }
 
     public HttpClientWrapper(String hostname, int port) {
+        this(hostname, port, null, null);
     }
 
     public String get(String path) {
-        throw new RuntimeException("Not yet implemented");
+        try {
+            GetMethod getMethod = new GetMethod(baseUrl() + path);
+            int returnCode = client().executeMethod(getMethod);
+            if (returnCode >= 200 && returnCode <= 200) {
+                return getMethod.getResponseBodyAsString();
+            }
+            throw new RuntimeException("The request could not be completed. The response was: " + returnCode);
+        } catch (IOException e) {
+            throw new RuntimeException("Connection pooped", e);
+        }
     }
 
-    static String scrub(String fullLink, String actualBegining) {
+    private String baseUrl() {
+        return String.format("http://%s:%d/go", hostname, port);
+    }
+
+    public static String scrub(String fullLink, String actualBegining) {
         return fullLink.substring(fullLink.indexOf(actualBegining));
+    }
+
+    private HttpClient client() {
+        HttpClient httpClient = new HttpClient();
+        if (username != null) {
+            httpClient.getParams().setAuthenticationPreemptive(true);
+            httpClient.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        }
+        return httpClient;
     }
 }
